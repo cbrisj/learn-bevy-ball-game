bab6ad436070dccccf0e00175a9da80586e0f5de
cyy <869212431@qq.com>
2023-09-12 13:18:36 +0800

    排序1

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -7,11 +7,15 @@ use systems::*;
 
 pub struct PlayPlugin;
 
+/// player_movement.before(confine_player_movement)
+/// confine_player_movement.after(player_movement)
+/// 两者等价
+/// 
+/// 还可以使用 .add_systems((player_movement, confine_player_movement).chain())
 impl Plugin for PlayPlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system(spawn_player)
-            .add_system(player_movement)
-            .add_system(confine_player_movement)
+            .add_systems((player_movement, confine_player_movement).chain())
             .add_system(enemy_hit_player)
             .add_system(player_hit_star);
     }
