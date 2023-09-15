68d19295d2a63d09e01e7751b228bc820a941190
cyy <869212431@qq.com>
2023-09-16 04:27:01 +0800

    排序

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -5,17 +5,32 @@ mod systems;
 
 use systems::*;
 
+// /// 声明为系统集
+// #[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+// pub struct MovementSystemSet;
+
+// #[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+// pub struct ConfinementSystemSet;
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub enum PlayerSystemSet {
+    Movement,
+    Confinement,
+}
+
 pub struct PlayPlugin;
 
 /// player_movement.before(confine_player_movement)
 /// confine_player_movement.after(player_movement)
 /// 两者等价
-/// 
+///
 /// 还可以使用 .add_systems((player_movement, confine_player_movement).chain())
 impl Plugin for PlayPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(spawn_player)
-            .add_systems((player_movement, confine_player_movement).chain())
+        app.configure_set(PlayerSystemSet::Movement.before(PlayerSystemSet::Confinement))
+            .add_startup_system(spawn_player)
+            .add_system(player_movement.in_set(PlayerSystemSet::Movement))
+            .add_system(confine_player_movement.in_set(PlayerSystemSet::Confinement))
             .add_system(enemy_hit_player)
             .add_system(player_hit_star);
     }
